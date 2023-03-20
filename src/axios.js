595ed8a03bcf8b02f977b86f1dd63ca156a1327c
LidiyaYem <102388444+LidiyaYem@users.noreply.github.com>
2023-03-20 23:30:13 +0200

    task-version-2

--- src/axios.js
+++ src/axios.js
@@ -6,9 +6,11 @@ export const searchedImage = input.value;
 
 const BASE_URL = `https://pixabay.com/api/`;
 const KEY = `34573956-5b25d4fd80103ca84cba18460`;
+let limit = 40;
+let page = 0;
 
 async function fetchImages(searchedImage) {
-
+    page += 1;
     try {
     const response = await axios.get(`${BASE_URL}`, {
 
@@ -18,9 +20,10 @@ async function fetchImages(searchedImage) {
         image_type: `photo`,
         orientation: `orientation`,
         safesearch: `true`,
+        per_page: limit,
+        page: page
     }
 });
-    // console.log(response);
     return response.data.hits;
     } catch (error) {
         console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,42 +1,47 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import {fetchImages} from './axios.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const input = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
+const gall = document.querySelector('.gallery');
+const button = document.querySelector('.load-more');
 
+let gallery = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+gallery.on('show.simplelightbox');
 
 input.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
-    // resetInput();
     e.preventDefault();
     const searchedImage = e.target.elements.searchQuery.value.trim();
     console.log(searchedImage);
 
     fetchImages(searchedImage)
-    .then(renderImage(searchedImage));
-    // (searchedImage => {
-      //   if (searchedImage = []) {
-      //   Notify.info(
-      //     'Sorry, there are no images matching your search query. Please try again.'
-      //   );
-      // } else {
-        
+    .then(searchedImage => {
+        if (!searchedImage) {
+        Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderImage(searchedImage);
 }
-// })
-// };
-
-// function resetInput() {
-//   refs.input.innerHTML = '';
-//   refs.gallery.innerHTML = '';
-// }
+})
+}
 
 function renderImage (searchedImage) {
   const markup = searchedImage
   .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `<a href="${largeImageURL}" alt="${tags}"><div class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <img src="${webformatURL}" alt="${tags} width = "100" height = "200" loading="lazy" />
       <div class="info">
         <p class="info-item">
           <b>Likes ${likes}</b>
@@ -54,6 +59,11 @@ function renderImage (searchedImage) {
     </div></a>`;
   }).join('');    
 
-  gallery.innerHTML = markup;
-  // refs.input.innerHTML = '';
+  gall.innerHTML = markup;
+  input.innerHTML = '';
+
+  button.classList.remove("hidden");
+
 }
+
+input.addEventListener('click', onButtonClick);
